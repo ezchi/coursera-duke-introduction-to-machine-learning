fbb0e2c4772d3ba65483ce6e0229275612334068
Enze Chi <enze.chi@gmail.com>
2018-11-25 08:06:20 +1100

    Add 2b MPL assignment requirement

---
+++ python/2b-mlp-assignment.py
@@ -0,0 +1,13 @@
+"""TensorFlow Assignment: Multi-Layer Perceptron (MLP)
+Duke Community Standard: By typing your name below, you are certifying that you have adhered to the Duke Community Standard in completing this assignment.
+
+Name:
+
+
+->
+### Multi-layer Perceptron
+
+Build a 2-layer MLP for MNIST digit classfication. Feel free to play around with the model architecture and see how the training time/performance changes, but to begin, try the following:
+
+Image (784 dimensions) -> fully connected layer (500 hidden units)  -> nonlinearity (ReLU) -> fully connected layer (100 hidden units) -> nonlinearity (ReLU) -> fully connected (10 hidden units) -> softmax
+"""
